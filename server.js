a5f01071be3d7f80196d15b0f764cdf331ae9940
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-10 12:15:00 +0000

    feature: Require user routes Create error handler Use json middleware on express Set subroute for users api

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const server = express();
+const userRouter = require('./users/userRouter');
 
+server.use(express.json());
 server.use(logger);
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -16,5 +19,9 @@ function logger(req, res, next) {
   );
   next();
 };
+
+function errorHandler(error, req, res, next) {
+  res.status(500).json({ message: "There was an error" })
+}
 
 module.exports = server;
