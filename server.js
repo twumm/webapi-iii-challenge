ad43b229615d52d097bd1babea449614333c9557
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-10 11:52:28 +0000

    feature: Add custom middleware to log information about each request with a timestamp to the console

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const server = express();
 
+server.use(logger);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
@@ -9,7 +11,10 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} request from ${req.url}}`
+  );
+  next();
 };
 
 module.exports = server;
