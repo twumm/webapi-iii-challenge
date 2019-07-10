7263c5b11ba73f7c5dae116d282efebffad6617b
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-10 13:57:53 +0000

    feature: Add postRouter

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const server = express();
 const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
 
 server.use(express.json());
 server.use(logger);
 server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
