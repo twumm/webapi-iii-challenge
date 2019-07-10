e568d584a1b9016554010fb9757b59ae33633129
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-10 14:43:37 +0000

    feature: Install, require and use cors as a middleware

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const server = express();
 const userRouter = require('./users/userRouter');
 const postRouter = require('./posts/postRouter');
 
 server.use(express.json());
+server.use(cors);
 server.use(logger);
 server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter);
